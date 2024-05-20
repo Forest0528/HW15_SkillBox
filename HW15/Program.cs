94cc91fdccdafd2bebb79ec8d995e276b0c5ab04
Faim.Badgutdinov <Faim.Badgutdinov@DVL-03>
2024-05-20 10:03:57 +0500

    Добавьте файлы проекта.

---
+++ HW15/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task PrintMessageAsync(string message, int delay)
+    {
+        await Task.Delay(delay);
+        Console.WriteLine($"{message} - Поток {Thread.CurrentThread.ManagedThreadId}");
+    }
+
+    static async Task Main(string[] args)
+    {
+        var tasks = new[]
+        {
+            PrintMessageAsync("Сообщение из потока 1", 1000),
+            PrintMessageAsync("Сообщение из потока 2", 2000),
+            PrintMessageAsync("Сообщение из потока 3", 3000),
+        };
+
+        await Task.WhenAll(tasks);
+
+        Console.WriteLine("Все задачи завершены.");
+        Console.ReadLine();
+    }
+}
